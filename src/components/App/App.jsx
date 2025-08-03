5197a7c4d27bfd7d07be575beaed736380eeb4fb
Lily Anderson <thatoanderson92@gmail.com>
2025-08-03 09:01:00 -0400

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,8 @@
+import Header from "../Header/Header.jsx";
+import "./App.css";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">Turbo Guacamole 🥑</h1>
+      <p className="header__paragraph">Find the perfect restaurant near you</p>
+    </header>
+  );
+};
+
+export default Header;
